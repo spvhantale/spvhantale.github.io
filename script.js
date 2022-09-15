41dc846acdec971a1039040ffedbd219a10ed1d1
spvhantale <vhantale510@gmail.com>
2022-09-15 11:46:32 +0530

    completed

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     })
 });
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "vhantale510@gmail.com",
+        Password : "22A2F9BBD0E11F20BEB785C37FBBA0744B92",
+        To : 'vhantale510@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Enquiry",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
